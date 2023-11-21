ac9321c9f582b93adcd515c6a54213e38d474321
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2023-11-20 19:25:53 -0600

    feat: minor style improvements to test out workflows

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# `static-resume`
+
+Used to render `type: "resume"` `static.json` files.
+
+----
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
 import DATA from '../../static.json';
 
+const { person, experience } = DATA.contents;
+
 export default function Home() {
   return (
-    <main className="">
-      <code>{DATA.type} v{DATA.version}</code>
-      <ul>
-        {DATA.contents.experience.map((e, i) => (
-          <li key={i}>{e.title} &mdash; {e.company_name}</li>
+    <main className="m-4">
+      <div>
+        <h1>{person.givenName} {person.familyName}</h1>
+        <address>
+          {person.address.streetAddress}
+          <br />
+          {person.address.addressLocality}, {person.address.addressRegion} {person.address.postalCode}
+        </address>
+        {person.email}
+        <br />
+        {person.telephone}
+      </div>
+      <ul className='divide-y divide-gray-100'>
+        {experience.map((e, i) => (
+          <li key={i} className='flex justify-between gap-x-6 py-5'>
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">{e.title}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{e.company_name}</p>
+            </div>
+          </li>
         ))}
       </ul>
     </main>
