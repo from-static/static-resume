cefcb256e7a75888b3299b7e852f031b50e305e7
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-03-21 19:06:59 -0500

    fix: use static.json to populate next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const { _static } = require('./static.json');
+
 const nextConfig = {
   output: 'export',
+  /**
+   * If the `static.json` file contains a `host` object, use the `base_path` value
+   * as the `basePath` for the Next.js application.
+   */
+  basePath: _static?.host?.base_path || undefined,
 }
 
 module.exports = nextConfig
